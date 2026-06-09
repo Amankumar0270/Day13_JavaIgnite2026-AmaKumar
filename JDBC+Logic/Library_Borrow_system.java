364eea4bb53b8c1ae820c4229dc368fdf6059a17
rite2harini <rite2harini@gmail.com>
2026-06-09 13:21:03 +0530

    Create Library_Borrow_system.java

---
+++ JDBC+Logic/Library_Borrow_system.java
@@ -0,0 +1,24 @@
+/*
+Create a Java program connected to database library_db.
+
+Table:
+
+book_id
+title
+available_copies
+Task:
+Allow user to input a book ID
+Check availability
+If available:
+Reduce copy count by 1
+Print “Book Issued”
+Else:
+Print “Not Available”
+Hint:
+
+Think:
+
+SELECT → check condition
+UPDATE → modify data
+Use if-else inside Java after ResultSet
+*/
